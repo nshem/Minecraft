9600ad1f40613eac06a8ce00625501f774c7b424
GreenNeptune <silentwisdom75@gmail.com>
2019-06-20 22:39:39 +0300

    aletring can mine function

--- js/entities/tool.js
+++ js/entities/tool.js
@@ -4,10 +4,26 @@ class Tool {
     this.ores = ores; //array
   }
 
+  canMine(pixel) {
+    // check if tools can mine some ore 
+    let isCompatible = this.ores.includes(pixel);
+    let canGetTheOre = false;
+    //get the current pixel 
+    let pixelType = GAME.currentPixel.type.slice(-1);
 
+    // check type of the pixels with split
+  
+    // set canGetTheOre to true if the ore is in the first in the row or have sky index above 
+    if (pixel.position[0] === 0) {
+      canGetTheOre = true;
+    } else if (pixel.position[0] - 1 === "sky") {
+      canGetTheOre = true;
+    }
 
-  canMine(clickedOre) {
-    return this.ores.includes(clickedOre);
+    if (isCompatible && canGetTheOre) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
-
